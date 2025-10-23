773ee1340403e79397738af380e74db903195b2c
Mechaspirit1 <pabloloschi200@gmail.com>
2025-10-23 15:46:48 -0300

    minor visual effect added pt 2

--- effects.js
+++ effects.js
@@ -5,14 +5,32 @@ const titleSel = document.querySelector("#title")
 let titleText = "Hello, i am Pablo...";
 let title = titleSel.textContent;
 let i = 0;
+let state = true;
 
 console.log(title);
 
 function typew(){
-    if (i<titleText.length){
-        titleSel.textContent += titleText.charAt(i);
-        i++
-        setTimeout(typew, 100);
+    if (state){ //efeito de escrita
+        if (i<titleText.length){
+            titleSel.textContent += titleText.charAt(i);
+            i++
+            setTimeout(typew, 100);
+        }
+        else{
+            state = false;
+            setTimeout(typew, 600);
+        }
+    }
+    else{ //efeito de delete
+        if (i > 0) {
+            titleSel.textContent = titleText.substring(0, i - 1);
+            i--;
+            setTimeout(typew, 50);
+        } 
+        else {
+            state = true;
+            setTimeout(typew, 500); //pausa antes de escrever o titulo novamente
+        }
     }
 }
 
